d61c2ccb5d396fef600044f550548876da82da39
tyskin K <kongqijin@outlook.com>
2017-07-31 18:11:17 +0100

    LK_convolutional2D_3
    LK_convolutional2D_3 v1
    Thumb, 234 bytes, Stack size 88 bytes Max Depth = 176
    ET:38339
    
    the worst, both in memory and time.
    
    v2:
    Thumb, 220 bytes, Stack size 64 bytes，Max Depth = 152
      ⇒ __aeabi_dmul ⇒ _double_epilogue ⇒ _double_round
    EXTIME:16390

--- STM32F7_CNN1/CNN_1_1/Src/main.c
+++ STM32F7_CNN1/CNN_1_1/Src/main.c
@@ -231,15 +231,52 @@ void LK_convolutional2D_2(LK_Accuarcy *input,		int isize_w, int isize_h,
 			*output =bias;  
 									for (int kh = 0; kh < ksize_h; kh++)
 									{
+										
 										for (int kw = 0; kw < ksize_w; kw++)
 										{
-											*output = *output + *(input +h*isize_w+w+ kh*isize_w + kw) * *(kernel + kh*ksize_w + kw);
 											
-											printf_s("the input is %f 	kernel is %f 	output is %f\r\n",*(input +h*isize_w+w+ kh*isize_w + kw),*(kernel + kh*ksize_w + kw),*output);
+											*output = *output + *(input +h*isize_w+w+ kh*isize_w + kw) * *(kernel + kh*ksize_w + kw);										
+											//	printf_s("the input is %f 	kernel is %f 	output is %f\r\n",*(input +h*isize_w+w+ kh*isize_w + kw),*(kernel + kh*ksize_w + kw),*output);
 										}				
 									} 
 			output++;
 		}
+	}
+}
+
+void LK_convolutional2D_3(LK_Accuarcy *input,		int isize_w, int isize_h,
+													LK_Accuarcy* kernel,  int ksize_w, int ksize_h, LK_Accuarcy bias,
+													LK_Accuarcy *output,	int osize_w, int osize_h
+													)
+{
+	for (int h = 0; h<osize_h; h++)
+	{		
+		for (int w = 0; w<osize_w; w++)
+		{			 
+			*output =bias;  
+									for (int kh = 0; kh < ksize_h; kh++)
+									{
+										for (int kw = 0; kw < ksize_w; kw++)
+										{
+											//	*output = *output + *(input +h*isize_w+w+ kh*isize_w + kw) * *(kernel + kh*ksize_w + kw);										
+											//	printf_s("the input is %f 	kernel is %f 	output is %f\r\n",*(input +h*isize_w+w+ kh*isize_w + kw),*(kernel + kh*ksize_w + kw),*output);
+											*output = *output +(*input )*(*kernel);
+										//	printf_s("the input is %f 	kernel is %f 	output is %f\r\n",*(input),*(kernel ),*output);
+											
+											kernel++;
+											input++;
+											
+											
+										}				
+										input+=isize_w-ksize_w;
+									} 
+			output++;
+			input=input-ksize_h*isize_w+ksize_w-2;// basic point of input
+									
+			kernel-=ksize_w*ksize_h;
+									
+		}
+		input+=(ksize_w-1);
 	}
 }
 
@@ -263,7 +300,7 @@ int main(void)
 	double a[5][5] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
 	double kernel[3][3] = { 1,0,1,0,1,0,1,0,1 };
 	double out[3][3] ;
-	LK_convolutional2D_2(&a[0][0],5,5,&kernel[0][0],3,3,0,&out[0][0],3,3);
+	LK_convolutional2D_3(&a[0][0],5,5,&kernel[0][0],3,3,0,&out[0][0],3,3);
 	 
 	LK_displayMatrix(&out[0][0],3,3," out");
 
